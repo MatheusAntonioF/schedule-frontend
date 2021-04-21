cb8e55e3d5b7e083f1d75aeb4b17dd68e54f492c
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2021-04-21 17:20:13 -0300

    [FEAT] Create authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RootProviders } from './hooks';
 
 import Routes from './routes';
 import { DefaultStyles } from './styles';
@@ -6,7 +7,9 @@ import { DefaultStyles } from './styles';
 const App = (): JSX.Element => {
   return (
     <DefaultStyles>
-      <Routes />
+      <RootProviders>
+        <Routes />
+      </RootProviders>
     </DefaultStyles>
   );
 };
---
+++ src/hooks/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AuthProvider } from './auth';
+
+const RootProviders: React.FC = ({ children }) => {
+  return <AuthProvider>{children}</AuthProvider>;
+};
+
+export { RootProviders };
