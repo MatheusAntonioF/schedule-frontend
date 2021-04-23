f0745fe9ea51d6db8a8fc8d539a23294651838ff
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2021-04-23 19:56:22 -0300

    [FEAT] Add calendar

--- src/pages/Schedules/index.tsx
+++ src/pages/Schedules/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 import { Wrapper, Container, Calendar } from './styles';
 import { TodayEvents } from './TodayEvents';
 
 const Schedules: React.FC = () => {
+  const buttonsHeaderCalendar = {
+    today: 'Hoje',
+    dayGridMonth: 'Mês',
+    week: 'week',
+    dayGridWeek: 'Semana',
+    list: 'list',
+  };
+
   return (
     <Wrapper>
       <Container>
-        <Calendar>Calendar</Calendar>
-        <TodayEvents>Eventos de hoje</TodayEvents>
+        <Calendar>
+          <FullCalendar
+            weekends
+            height="100%"
+            contentHeight="100%"
+            initialView="dayGridMonth"
+            locale="pt-br"
+            plugins={[dayGridPlugin]}
+            buttonText={buttonsHeaderCalendar}
+          />
+        </Calendar>
+        <TodayEvents />
       </Container>
     </Wrapper>
   );
--- src/pages/Schedules/styles.ts
+++ src/pages/Schedules/styles.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import { transparentize } from 'polished';
+import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -31,5 +32,61 @@ export const Calendar = styled.div`
   width: 100%;
   height: 100%;
 
-  background: ${({ theme: { pallete } }) => pallete.darker.light};
+  padding: 0.5rem 0.7rem 0.5rem;
+
+  border-radius: 5px;
+
+  ${({ theme: { pallete } }) => css`
+    .fc-toolbar-title {
+      color: ${pallete.primary.main};
+    }
+
+    .fc-today-button,
+    .fc-button,
+    .fc-button-primary {
+      background: ${pallete.primary.main};
+
+      transition: all 250ms ease-in-out;
+
+      border: none;
+    }
+
+    .fc-today-button {
+      &:disabled {
+        background: ${transparentize(0.8, pallete.primary.main)};
+        transform: scale(1.05);
+      }
+
+      &:hover {
+        background: ${transparentize(0.4, pallete.primary.main)};
+        transform: scale(1.05);
+      }
+    }
+
+    .fc-prev-button {
+      margin-right: 0.1rem;
+
+      &:hover {
+        background: ${transparentize(0.4, pallete.primary.main)};
+        transform: translateX(-5px);
+      }
+    }
+
+    .fc-next-button {
+      margin-left: 0.1rem;
+
+      &:hover {
+        background: ${transparentize(0.4, pallete.primary.main)};
+        transform: translateX(5px);
+      }
+    }
+
+    .fc-day-today {
+      background: ${transparentize(0.8, pallete.primary.main)} !important;
+    }
+
+    color: ${pallete.text.main};
+
+    background: ${pallete.darker.light};
+  `}
 `;
